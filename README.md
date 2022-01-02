c55bc6dc3cf48b42bd90fe2117ec82e2efa3b5e0
Emma Jellema Butler <emma.jellemabutler@gmail.com>
2022-01-03 00:53:08 +0200

    update

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # bathealth
 
-### Disclaimer: I haven't checked the compatibility of this script on anything besides my main laptop, a Dell Inspiron 15 5100 running Arch, on Alacritty. I see no reason it wouldn't work on anything else, but it is untested 
+###### Disclaimer: I haven't checked the compatibility of this script on anything besides my main laptop, a Dell Inspiron 15 5100 running Arch, on Alacritty. I see no reason it wouldn't work on anything else, but it is untested 
+
+**To either run this script quickly or sample it's output, feel free to run:**
+`curl -s https://raw.githubusercontent.com/jemhop/bathealth/main/bathealth.rb | ruby`
 
 A simple Ruby script that fetches your battery health from UPower and prints it in a readable way. It defaults to fetching the health for battery-BAT0,
 but if supplied with the flag -a it will give you a selection menu of all your power devices.
 
 It prints some little 2012 style messages by default, these can be changed super easily. 
 
-
+*Obviously requires Ruby and UPower*
--- bathealth.rb
+++ bathealth.rb
@@ -1,28 +1,32 @@
 #!/usr/bin/ruby
 
-def put_bat_info(bat_path)
+def put_bat_info(bat_path, debug_perc)
 	output = `upower -i #{bat_path} | grep capacity`.gsub(/\s+/, "") 
 	index = output.index(':')
 	colour = ""
 	text = ""
-	percentage = output[index+1...-1].to_f
+	if debug_perc == -1
+		percentage = output[index+1...-1].to_f
+	else
+		percentage = debug_perc
+	end
 
 	#percentage = 100
 
 	case percentage 
-	when 1..15
+	when 0..20
 		colour = "196"
 		text = "Time for a new battery >:("
-	when 16..25 
+	when 21..40 
 		colour = "202"
 		text = "Go to power jail :("
-	when 26..50 
+	when 41..60 
 		colour = "226"
 		text = "Doin okay I guess :|"
-	when 51..75
+	when 61..80
 		colour = "76"
 		text = "Lookin pretty good :)"
-	when 76..100 
+	when 81..100 
 		colour = "46"	
 		text = "Flawless faultless awesome cool >:D"
 	end
@@ -32,38 +36,51 @@ def put_bat_info(bat_path)
 	puts "\x1b[7;38;5;#{colour}m#{text}"
 end
 
+def get_power_choice()
+	bat_sel = ''
+	for i in 0..bat_array.size-1
+		puts "\x1b[1m#{i+1}.)\x1b[0m #{bat_array[i]}"
+	end
 
+	input = Integer(gets)
+
+
+
+
+
+	if input > bat_array.size 		
+		puts 'Invalid input'
+		return 'invalid'
+	end
+	
+	for i in 0..bat_array.size-1
+		if i == input-1 
+			bat_sel = bat_array[i]
+		end
+	end
+
+	return bat_sel
+end
+
+debug_perc = -1
 
 bat_list = `upower -e`
-
 bat_array = bat_list.split("\n")
-
 bat_sel = '/org/freedesktop/UPower/devices/battery_BAT0'
 
-if ARGV.length > 0 && ARGV[0] == '-a'
-ARGV.clear
+if ARGV.length > 0
 
-for i in 0..bat_array.size-1
-	puts "\x1b[1m#{i+1}.)\x1b[0m #{bat_array[i]}"
-end
-
-input = Integer(gets)
-
-
-
-
-if input > bat_array.size 		
-	puts 'Invalid input'
-	return
-end
-
-for i in 0..bat_array.size-1
-	if i == input-1 
-		bat_sel = bat_array[i]
+	if ARGV[0] == '-a'
+		bat_sel = print_power_options
+		puts bat_sel
+	else
+		debug_perc = ARGV[0].to_f
 	end
-end
+	ARGV.clear
 end
 
-put_bat_info(bat_sel)
+put_bat_info(bat_sel, debug_perc)
+
+print "\x1b[0m"
 
 
